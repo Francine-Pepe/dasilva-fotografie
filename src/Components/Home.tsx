feea4664a854cd914fc48376ec68cde0a4643738
Francine <franmelopepe2@gmail.com>
2023-07-12 11:22:27 +0200

    navbar component added to home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,14 @@
 import "../Styles/styles.sass";
 import DarkLightMode from "./DarkLightMode";
+import Navbar from "./Navbar";
 
 function Home() {
   return (
     <div className="home-container">
       <div className="home-background">
+        <div className="home-navbar">
+          <Navbar />
+        </div>
         <DarkLightMode />
       </div>
     </div>
