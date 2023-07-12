cccf4f7340b5a457b8bdcdfdcdd4f928b5f7c8cf
Francine <franmelopepe2@gmail.com>
2023-07-12 11:22:07 +0200

    navbar component created

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import Logo from "../Assets/dasilva_logo.png";
+import { navLinks } from "../data";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar-container">
+      <div className="logo">
+        <NavLink to={"/home"}>
+          <img src={Logo} alt="DaSilva-logo" />
+        </NavLink>
+      </div>
+      <nav className="navbar-home">
+        {navLinks.map((data, index) => {
+          return (
+            <ul key={index}>
+              <li>
+                <NavLink to={data.link}>{data.name}</NavLink>
+              </li>
+            </ul>
+          );
+        })}
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
