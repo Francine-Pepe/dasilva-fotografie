b1da4cf9d8f9d5529a279637eeded19547433457
Francine <franmelopepe2@gmail.com>
2023-07-12 11:23:51 +0200

    router configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.css';
-import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route index path="home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
