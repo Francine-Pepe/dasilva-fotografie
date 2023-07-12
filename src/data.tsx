24eda3603a57810b13df6a53ea9a610cd97b97f0
Francine <franmelopepe2@gmail.com>
2023-07-12 11:23:18 +0200

    navdata

---
+++ src/data.tsx
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        name: "Home",
+        link: "/home"
+    },
+    {
+        name: "Work",
+        link: "/work"
+    },
+    {
+        name: "About",
+        link: "/about"
+    },
+    {
+        name: "Contact",
+        link: "/contact"
+    },
+];
