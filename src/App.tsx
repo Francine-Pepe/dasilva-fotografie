44640614909cdb571835f6725782deeda6d039e2
Francine <franmelopepe2@gmail.com>
2023-07-21 09:44:51 +0200

    router configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import "./App.sass";
 import Home from "./Components/Home";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
+import Work from "./Components/Work";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import DarkLightMode from "./Components/DarkLightMode";
+import NavbarVertical from "./Components/Navbar/NavbarVertical";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />
+        <DarkLightMode />
+        {/* <Home /> */}
         <Routes>
-          <Route index path="home" element={<Home />} />
+          <Route index path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
