f7f0cb95c67b214df6c679da58d1baa1505c7d7d
Francine <franmelopepe2@gmail.com>
2023-07-21 09:46:07 +0200

    darkmode implemented in home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,15 +1,15 @@
 import "../Styles/styles.sass";
 import DarkLightMode from "./DarkLightMode";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar/Navbar";
 
 function Home() {
   return (
     <div className="home-container">
       <div className="home-background">
         <div className="home-navbar">
+          <DarkLightMode />
           <Navbar />
         </div>
-        <DarkLightMode />
       </div>
     </div>
   );
