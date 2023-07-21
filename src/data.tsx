90b93384922bb1e9c615b4392b6b268df3f932da
Francine <franmelopepe2@gmail.com>
2023-07-21 09:45:04 +0200

    new data added

--- src/data.tsx
+++ src/data.tsx
@@ -1,18 +1,45 @@
 export const navLinks = [
-    {
-        name: "Home",
-        link: "/home"
-    },
-    {
-        name: "Work",
-        link: "/work"
-    },
-    {
-        name: "About",
-        link: "/about"
-    },
-    {
-        name: "Contact",
-        link: "/contact"
-    },
+  {
+    name: "Home",
+    link: "/home",
+  },
+  {
+    name: "Work",
+    link: "/work",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+];
+
+export const navLinksSubMenu = [
+  {
+    name: "Animals",
+    link: "/work/animals",
+  },
+  {
+    name: "Flowers",
+    link: "/work/flowers",
+  },
+  {
+    name: "Food",
+    link: "/work/food",
+  },
+  {
+    name: "Landscape",
+    link: "/work/landscape",
+  },
+  {
+    name: "Portrait",
+    link: "/work/portrait",
+  },
+  {
+    name: "Product",
+    link: "/work/product",
+  },
 ];
