1bb05752e7bf046c0d3895cf470d0898b25987e3
Francine <franmelopepe2@gmail.com>
2023-07-21 09:45:49 +0200

    contact component created

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='contact-container'>
+      {/* <NavbarVertical /> */}
+    </div>
+  )
+}
+
+export default Contact
