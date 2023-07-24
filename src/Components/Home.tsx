5e2122003008728937187681cd7bfbee6a1fff4e
Francine <franmelopepe2@gmail.com>
2023-07-24 14:45:57 +0200

    footer home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,12 +3,22 @@ import DarkLightMode from "./DarkLightMode";
 import Navbar from "./Navbar/Navbar";
 
 function Home() {
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
   return (
     <div className="home-container">
       <div className="home-background">
         <div className="home-navbar">
           <DarkLightMode />
           <Navbar />
+          <footer className="footer-home">
+            <section>
+              <small>
+                &copy; Copyright {getCurrentYear()} - Da Silva Fotografie
+              </small>
+            </section>
+          </footer>
         </div>
       </div>
     </div>
