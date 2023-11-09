d41dfd94d4974de7187151cfa96d0e905de0cd15
Francine <franmelopepe2@gmail.com>
2023-11-09 20:03:30 +0100

    container props

---
+++ src/Components/ContainersProps.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ContainersProps() {
+  return (
+    <main className="contact-container">
+      <div className="contact-content">
+        <div className="contact-address"></div>
+        <div className="contact-form"></div>
+      </div>
+    </main>
+  );
+}
+
+export default ContainersProps;
