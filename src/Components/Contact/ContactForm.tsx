ec6ca805df97b934680ff8d40fa5b114935911b5
Francine <franmelopepe2@gmail.com>
2023-11-09 20:02:34 +0100

    contact form component

---
+++ src/Components/Contact/ContactForm.tsx
@@ -0,0 +1,75 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { ContactFormData } from "../../data";
+import swal from "sweetalert";
+
+function ContactForm() {
+  const form = useRef(null);
+
+  const sendEmail = (e: {
+    target: any | HTMLFormElement;
+    preventDefault: () => void;
+  }) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_2q4mln6",
+        "template_c3vunb6",
+        e.target,
+        "user_YeUwC6WQsK1xiMGVBrKzC"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <main className="contact-content">
+      <form ref={form} onSubmit={sendEmail} className="contact-form">
+        {ContactFormData.map(
+          ({ id, title, htmlFor, name, type, placeholder }) => {
+            return (
+              <div className="contact-inputs" key={id}>
+                <label htmlFor={htmlFor}>{title}</label>
+                <input
+                  type={type}
+                  name={name}
+                  placeholder={placeholder}
+                  required
+                  min="2"
+                />
+              </div>
+            );
+          }
+        )}
+        <div className="contact-inputs">
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required />
+        </div>
+        <div className="contact-button-container">
+          <button
+            className="contact-button"
+            type="submit"
+            value="Send"
+            onClick={() =>
+              swal({
+                text: "Thanks for contacting me! I will get back to you soon! 📧",
+                icon: "success",
+              })
+            }
+          >
+            Send 
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+}
+
+export default ContactForm;
