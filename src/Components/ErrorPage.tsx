bb713e3cdf112e8750769cf9316609e7c50c3656
Francine <franmelopepe2@gmail.com>
2024-01-23 15:06:29 +0100

    errorpage component created

---
+++ src/Components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div className="error-page">
+        <h2>Error 404</h2>
+        <h2>Sorry, page not found 😞 </h2>
+    </div>
+  )
+}
+
+export default ErrorPage;
