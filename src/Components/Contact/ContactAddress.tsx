27766c2fae7eefaa06da55dd5efaadd865fb8d2c
Francine <franmelopepe2@gmail.com>
2024-01-29 14:17:45 +0100

    corrected identation

--- src/Components/Contact/ContactAddress.tsx
+++ src/Components/Contact/ContactAddress.tsx
@@ -7,7 +7,9 @@ function ContactAddress() {
         return (
           <div className="contact-address" key={index}>
             <p>{data.title}</p>
-            <a href={data.mailTo}><p>{data.email}</p></a>
+            <a href={data.mailTo}>
+              <p>{data.email}</p>
+            </a>
             <p>{data.phone}</p>
             <p>{data.country}</p>
           </div>
